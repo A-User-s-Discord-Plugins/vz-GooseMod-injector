34525b68c27d5d3b24efa7a09d0399f845566d50
abUwUser <pedro.santos.cartaxo@gmail.com>
2020-12-25 09:36:58 -0300

    Yes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Plugin } = require('@vizality/entities')
 
-module.exports = class BDCompat extends Plugin {
+module.exports = class GooseModInjector extends Plugin {
     async onStart() {
         eval(await(await fetch('https://api.goosemod.com/inject.js')).text())
     }
